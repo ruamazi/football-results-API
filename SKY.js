eb92737a28e003edfa3e4f803a90efe6961e0d1b
. <barcalover016@gmail.com>
2024-11-20 21:55:40 +0100

    added sky endpoint

---
+++ SKY.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export const SkyAPI = async (req, res) => {
+  const url = "https://www.skysports.com/football-results";
+  try {
+    const resp = await axios.get(url);
+    const html = resp.data;
+    const $ = cheerio.load(html);
+    const matches = [];
+    $(".fixres__item").each((index, element) => {
+      const matchUrl = $(element).find("a").attr("href");
+      const teams = {
+        home: $(element)
+          .find(".matches__participant--side1 .swap-text__target")
+          .text()
+          .trim(),
+        away: $(element)
+          .find(".matches__participant--side2 .swap-text__target")
+          .text()
+          .trim(),
+      };
+      const scores = {
+        home: $(element)
+          .find(".matches__teamscores-side:first-child")
+          .text()
+          .trim(),
+        away: $(element)
+          .find(".matches__teamscores-side:last-child")
+          .text()
+          .trim(),
+      };
+      matches.push({ matchUrl, teams, scores });
+    });
+    res.json(matches);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import express from "express";
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { BBCAPI } from "./BBC.js";
+import { SkyAPI } from "./SKY.js";
 
 const app = express();
 const port = process.env.PORT || 3038;
 
 app.get("/bbc", BBCAPI);
+app.get("/sky", SkyAPI);
 
-app.get("/status", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ status: "OK" });
 });
 
